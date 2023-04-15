e0dfdc31cdffd04396d04476a742e0ffe0f13cb8
iammaga <81741344+iammaga@users.noreply.github.com>
2023-04-15 16:00:47 +0700

    closes #6: Create view and pass array tasks (#7)
    * closes #6: Create view and pass array tasks
    
    * Changed route & seeder files

---
+++ resources/views/tasks/tasks.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List of tasks</title>
+</head>
+
+<body>
+    <?php foreach ($tasks as $task): ?>
+        <h2><a href="/tasks/<?php echo $task['id'] ?>"><?php echo $task['title'] ?></a></h2>
+        <?php echo $task['body'] ?>
+    <?php endforeach; ?>
+</body>
+
+</html>
