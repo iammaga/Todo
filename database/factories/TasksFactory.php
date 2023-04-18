4fde982eb03e0a3b7a481cf5c6d2c73a8437942f
iammaga <81741344+iammaga@users.noreply.github.com>
2023-04-18 18:47:41 +0700

    closes #11: Create task factory (#14)

---
+++ database/factories/TasksFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TasksFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->sentence(),
+            'created_at' => $this->faker->date(),
+        ];
+    }
+}
