1396601bc000170bc50c914c5902ec3fa52f7725
Maga <muxammad.zikirzoda@gmail.com>
2023-04-30 17:50:47 +0700

    closes #28: added padding

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,5 +1,5 @@
-<nav class="ml-0 m-4">
-    <div class="flex">
+<nav class="ml-1 m-4">
+    <div class="flex p-4">
         <a href="/"><h1 class="text-3xl font-bold">2DO</h1></a>
         <ul class="list-none flex items-center ml-auto mr-auto">
             <li class="ml-3"><a href="/users">Users</a></li>
